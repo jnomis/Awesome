54822d2e5ce365179242eb8c67058af28baa24c7
Simon <simon.hergenhan@baxter-academy.org>
2016-02-25 09:54:16 -0500

    AWESOME 1

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject currentCheckpoint;
+
+	private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+	private bool moveRight;
+
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+		
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown (KeyCode.LeftArrow) && (grounded)) 
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+		}
+
+		if(moveRight)
+		{
+			transform.localScale = new Vector3 (-1f, 1f, 1f);
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		} else {
+			transform.localScale = new Vector3 (1f, 1f, 1f);
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		}
+
+		if(Input.GetKeyDown (KeyCode.RightArrow))
+		{
+			moveRight = !moveRight;
+		}
+
+	}
+}
